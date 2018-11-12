c4f52b6332795ace53dab23113ecef427a73a414
pzb <393733352@qq.com>
2018-11-12 11:08:26 +0800

    更新

--- src/test/java/com/puzhibing/StockAnalysis/StockAnalysisApplicationTests.java
+++ src/test/java/com/puzhibing/StockAnalysis/StockAnalysisApplicationTests.java
@@ -1,9 +1,15 @@
 package com.puzhibing.StockAnalysis;
 
+import java.io.IOException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.puzhibing.StockAnalysis.service.CrawlingCompanyDate;
+import com.puzhibing.StockAnalysis.service.impl.CrawlingCompanyDateImpl;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +17,13 @@ public class StockAnalysisApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		CrawlingCompanyDate cr = new CrawlingCompanyDateImpl();
+		try {
+			cr.crawlingShanghai("B", "OI", "IO", "IKOL");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
