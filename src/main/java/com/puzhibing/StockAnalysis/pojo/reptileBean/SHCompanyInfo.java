f7afcde1384251f73a3c4c6b2a1a0ce801720607
pzb <393733352@qq.com>
2018-11-20 14:49:55 +0800

    添加爬取封装数据对象

---
+++ src/main/java/com/puzhibing/StockAnalysis/pojo/reptileBean/SHCompanyInfo.java
@@ -0,0 +1,313 @@
+package com.puzhibing.StockAnalysis.pojo.reptileBean;
+
+public class SHCompanyInfo {
+
+	private String ENGLISH_ABBR;
+	
+	private String LEGAL_REPRESENTATIVE;
+	
+	private String REPR_PHONE;
+	
+	private String CSRC_CODE_DESC;
+	
+	private String E_MAIL_ADDRESS;
+	
+	private String SSE_CODE_DESC;
+	
+	private String WWW_ADDRESS;
+	
+	private String SECURITY_CODE_A;
+	
+	private String SECURITY_CODE_B;
+	
+	private String COMPANY_CODE;
+	
+	private String OFFICE_ADDRESS;
+	
+	private String SECURITY_ABBR_A;
+	
+	private String COMPANY_ABBR;
+	
+	private String AREA_NAME_DESC;
+	
+	private String STATE_CODE_A_DESC;
+	
+	private String CSRC_MIDDLE_CODE_DESC;
+	
+	private String FULLNAME;
+	
+	private String FOREIGN_LISTING_ADDRESS;
+	
+	private String COMPANY_ADDRESS;
+	
+	private String STATE_CODE_B_DESC;
+	
+	private String CSRC_GREAT_CODE_DESC;
+	
+	private String SCU_TYPE;
+	
+	private String SECURITY_30_DESC;
+	
+	private String CHANGEABLE_BOND_CODE;
+	
+	private String FOREIGN_LISTING_DESC;
+	
+	private String CHANGEABLE_BOND_ABBR;
+	
+	private String SECURITY_CODE_A_SZ;
+	
+	private String FULL_NAME_IN_ENGLISH;
+	
+	private String OFFICE_ZIP;
+
+	public String getENGLISH_ABBR() {
+		return ENGLISH_ABBR;
+	}
+
+	public void setENGLISH_ABBR(String eNGLISH_ABBR) {
+		ENGLISH_ABBR = eNGLISH_ABBR;
+	}
+
+	public String getLEGAL_REPRESENTATIVE() {
+		return LEGAL_REPRESENTATIVE;
+	}
+
+	public void setLEGAL_REPRESENTATIVE(String lEGAL_REPRESENTATIVE) {
+		LEGAL_REPRESENTATIVE = lEGAL_REPRESENTATIVE;
+	}
+
+	public String getREPR_PHONE() {
+		return REPR_PHONE;
+	}
+
+	public void setREPR_PHONE(String rEPR_PHONE) {
+		REPR_PHONE = rEPR_PHONE;
+	}
+
+	public String getCSRC_CODE_DESC() {
+		return CSRC_CODE_DESC;
+	}
+
+	public void setCSRC_CODE_DESC(String cSRC_CODE_DESC) {
+		CSRC_CODE_DESC = cSRC_CODE_DESC;
+	}
+
+	public String getE_MAIL_ADDRESS() {
+		return E_MAIL_ADDRESS;
+	}
+
+	public void setE_MAIL_ADDRESS(String e_MAIL_ADDRESS) {
+		E_MAIL_ADDRESS = e_MAIL_ADDRESS;
+	}
+
+	public String getSSE_CODE_DESC() {
+		return SSE_CODE_DESC;
+	}
+
+	public void setSSE_CODE_DESC(String sSE_CODE_DESC) {
+		SSE_CODE_DESC = sSE_CODE_DESC;
+	}
+
+	public String getWWW_ADDRESS() {
+		return WWW_ADDRESS;
+	}
+
+	public void setWWW_ADDRESS(String wWW_ADDRESS) {
+		WWW_ADDRESS = wWW_ADDRESS;
+	}
+
+	public String getSECURITY_CODE_A() {
+		return SECURITY_CODE_A;
+	}
+
+	public void setSECURITY_CODE_A(String sECURITY_CODE_A) {
+		SECURITY_CODE_A = sECURITY_CODE_A;
+	}
+
+	public String getSECURITY_CODE_B() {
+		return SECURITY_CODE_B;
+	}
+
+	public void setSECURITY_CODE_B(String sECURITY_CODE_B) {
+		SECURITY_CODE_B = sECURITY_CODE_B;
+	}
+
+	public String getCOMPANY_CODE() {
+		return COMPANY_CODE;
+	}
+
+	public void setCOMPANY_CODE(String cOMPANY_CODE) {
+		COMPANY_CODE = cOMPANY_CODE;
+	}
+
+	public String getOFFICE_ADDRESS() {
+		return OFFICE_ADDRESS;
+	}
+
+	public void setOFFICE_ADDRESS(String oFFICE_ADDRESS) {
+		OFFICE_ADDRESS = oFFICE_ADDRESS;
+	}
+
+	public String getSECURITY_ABBR_A() {
+		return SECURITY_ABBR_A;
+	}
+
+	public void setSECURITY_ABBR_A(String sECURITY_ABBR_A) {
+		SECURITY_ABBR_A = sECURITY_ABBR_A;
+	}
+
+	public String getCOMPANY_ABBR() {
+		return COMPANY_ABBR;
+	}
+
+	public void setCOMPANY_ABBR(String cOMPANY_ABBR) {
+		COMPANY_ABBR = cOMPANY_ABBR;
+	}
+
+	public String getAREA_NAME_DESC() {
+		return AREA_NAME_DESC;
+	}
+
+	public void setAREA_NAME_DESC(String aREA_NAME_DESC) {
+		AREA_NAME_DESC = aREA_NAME_DESC;
+	}
+
+	public String getSTATE_CODE_A_DESC() {
+		return STATE_CODE_A_DESC;
+	}
+
+	public void setSTATE_CODE_A_DESC(String sTATE_CODE_A_DESC) {
+		STATE_CODE_A_DESC = sTATE_CODE_A_DESC;
+	}
+
+	public String getCSRC_MIDDLE_CODE_DESC() {
+		return CSRC_MIDDLE_CODE_DESC;
+	}
+
+	public void setCSRC_MIDDLE_CODE_DESC(String cSRC_MIDDLE_CODE_DESC) {
+		CSRC_MIDDLE_CODE_DESC = cSRC_MIDDLE_CODE_DESC;
+	}
+
+	public String getFULLNAME() {
+		return FULLNAME;
+	}
+
+	public void setFULLNAME(String fULLNAME) {
+		FULLNAME = fULLNAME;
+	}
+
+	public String getFOREIGN_LISTING_ADDRESS() {
+		return FOREIGN_LISTING_ADDRESS;
+	}
+
+	public void setFOREIGN_LISTING_ADDRESS(String fOREIGN_LISTING_ADDRESS) {
+		FOREIGN_LISTING_ADDRESS = fOREIGN_LISTING_ADDRESS;
+	}
+
+	public String getCOMPANY_ADDRESS() {
+		return COMPANY_ADDRESS;
+	}
+
+	public void setCOMPANY_ADDRESS(String cOMPANY_ADDRESS) {
+		COMPANY_ADDRESS = cOMPANY_ADDRESS;
+	}
+
+	public String getSTATE_CODE_B_DESC() {
+		return STATE_CODE_B_DESC;
+	}
+
+	public void setSTATE_CODE_B_DESC(String sTATE_CODE_B_DESC) {
+		STATE_CODE_B_DESC = sTATE_CODE_B_DESC;
+	}
+
+	public String getCSRC_GREAT_CODE_DESC() {
+		return CSRC_GREAT_CODE_DESC;
+	}
+
+	public void setCSRC_GREAT_CODE_DESC(String cSRC_GREAT_CODE_DESC) {
+		CSRC_GREAT_CODE_DESC = cSRC_GREAT_CODE_DESC;
+	}
+
+	public String getSCU_TYPE() {
+		return SCU_TYPE;
+	}
+
+	public void setSCU_TYPE(String sCU_TYPE) {
+		SCU_TYPE = sCU_TYPE;
+	}
+
+	public String getSECURITY_30_DESC() {
+		return SECURITY_30_DESC;
+	}
+
+	public void setSECURITY_30_DESC(String sECURITY_30_DESC) {
+		SECURITY_30_DESC = sECURITY_30_DESC;
+	}
+
+	public String getCHANGEABLE_BOND_CODE() {
+		return CHANGEABLE_BOND_CODE;
+	}
+
+	public void setCHANGEABLE_BOND_CODE(String cHANGEABLE_BOND_CODE) {
+		CHANGEABLE_BOND_CODE = cHANGEABLE_BOND_CODE;
+	}
+
+	public String getFOREIGN_LISTING_DESC() {
+		return FOREIGN_LISTING_DESC;
+	}
+
+	public void setFOREIGN_LISTING_DESC(String fOREIGN_LISTING_DESC) {
+		FOREIGN_LISTING_DESC = fOREIGN_LISTING_DESC;
+	}
+
+	public String getCHANGEABLE_BOND_ABBR() {
+		return CHANGEABLE_BOND_ABBR;
+	}
+
+	public void setCHANGEABLE_BOND_ABBR(String cHANGEABLE_BOND_ABBR) {
+		CHANGEABLE_BOND_ABBR = cHANGEABLE_BOND_ABBR;
+	}
+
+	public String getSECURITY_CODE_A_SZ() {
+		return SECURITY_CODE_A_SZ;
+	}
+
+	public void setSECURITY_CODE_A_SZ(String sECURITY_CODE_A_SZ) {
+		SECURITY_CODE_A_SZ = sECURITY_CODE_A_SZ;
+	}
+
+	public String getFULL_NAME_IN_ENGLISH() {
+		return FULL_NAME_IN_ENGLISH;
+	}
+
+	public void setFULL_NAME_IN_ENGLISH(String fULL_NAME_IN_ENGLISH) {
+		FULL_NAME_IN_ENGLISH = fULL_NAME_IN_ENGLISH;
+	}
+
+	public String getOFFICE_ZIP() {
+		return OFFICE_ZIP;
+	}
+
+	public void setOFFICE_ZIP(String oFFICE_ZIP) {
+		OFFICE_ZIP = oFFICE_ZIP;
+	}
+
+	@Override
+	public String toString() {
+		return "SHCompanyInfo [ENGLISH_ABBR=" + ENGLISH_ABBR + ", LEGAL_REPRESENTATIVE=" + LEGAL_REPRESENTATIVE
+				+ ", REPR_PHONE=" + REPR_PHONE + ", CSRC_CODE_DESC=" + CSRC_CODE_DESC + ", E_MAIL_ADDRESS="
+				+ E_MAIL_ADDRESS + ", SSE_CODE_DESC=" + SSE_CODE_DESC + ", WWW_ADDRESS=" + WWW_ADDRESS
+				+ ", SECURITY_CODE_A=" + SECURITY_CODE_A + ", SECURITY_CODE_B=" + SECURITY_CODE_B + ", COMPANY_CODE="
+				+ COMPANY_CODE + ", OFFICE_ADDRESS=" + OFFICE_ADDRESS + ", SECURITY_ABBR_A=" + SECURITY_ABBR_A
+				+ ", COMPANY_ABBR=" + COMPANY_ABBR + ", AREA_NAME_DESC=" + AREA_NAME_DESC + ", STATE_CODE_A_DESC="
+				+ STATE_CODE_A_DESC + ", CSRC_MIDDLE_CODE_DESC=" + CSRC_MIDDLE_CODE_DESC + ", FULLNAME=" + FULLNAME
+				+ ", FOREIGN_LISTING_ADDRESS=" + FOREIGN_LISTING_ADDRESS + ", COMPANY_ADDRESS=" + COMPANY_ADDRESS
+				+ ", STATE_CODE_B_DESC=" + STATE_CODE_B_DESC + ", CSRC_GREAT_CODE_DESC=" + CSRC_GREAT_CODE_DESC
+				+ ", SCU_TYPE=" + SCU_TYPE + ", SECURITY_30_DESC=" + SECURITY_30_DESC + ", CHANGEABLE_BOND_CODE="
+				+ CHANGEABLE_BOND_CODE + ", FOREIGN_LISTING_DESC=" + FOREIGN_LISTING_DESC + ", CHANGEABLE_BOND_ABBR="
+				+ CHANGEABLE_BOND_ABBR + ", SECURITY_CODE_A_SZ=" + SECURITY_CODE_A_SZ + ", FULL_NAME_IN_ENGLISH="
+				+ FULL_NAME_IN_ENGLISH + ", OFFICE_ZIP=" + OFFICE_ZIP + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/puzhibing/StockAnalysis/pojo/reptileBean/SZCompanyInfo.java
@@ -0,0 +1,194 @@
+package com.puzhibing.StockAnalysis.pojo.reptileBean;
+
+public class SZCompanyInfo {
+
+	private String gsqc;
+	
+	private String ywqc;
+	
+	private String zcdz;
+	
+	private String agdm;
+	
+	private String agjc;
+	
+	private String agssrq;
+	
+	private String agzgb;
+	
+	private String agltgb;
+	
+	private String bgdm;
+	
+	private String bgjc;
+	
+	private String bgssrq;
+	
+	private String bgzgb;
+	
+	private String bgltgb;
+	
+	private String dldq;
+	
+	private String sheng;
+	
+	private String shi;
+	
+	private String sshymc;
+	
+	private String http;
+
+	public String getGsqc() {
+		return gsqc;
+	}
+
+	public void setGsqc(String gsqc) {
+		this.gsqc = gsqc;
+	}
+
+	public String getYwqc() {
+		return ywqc;
+	}
+
+	public void setYwqc(String ywqc) {
+		this.ywqc = ywqc;
+	}
+
+	public String getZcdz() {
+		return zcdz;
+	}
+
+	public void setZcdz(String zcdz) {
+		this.zcdz = zcdz;
+	}
+
+	public String getAgdm() {
+		return agdm;
+	}
+
+	public void setAgdm(String agdm) {
+		this.agdm = agdm;
+	}
+
+	public String getAgjc() {
+		return agjc;
+	}
+
+	public void setAgjc(String agjc) {
+		this.agjc = agjc;
+	}
+
+	public String getAgssrq() {
+		return agssrq;
+	}
+
+	public void setAgssrq(String agssrq) {
+		this.agssrq = agssrq;
+	}
+
+	public String getAgzgb() {
+		return agzgb;
+	}
+
+	public void setAgzgb(String agzgb) {
+		this.agzgb = agzgb;
+	}
+
+	public String getAgltgb() {
+		return agltgb;
+	}
+
+	public void setAgltgb(String agltgb) {
+		this.agltgb = agltgb;
+	}
+
+	public String getBgdm() {
+		return bgdm;
+	}
+
+	public void setBgdm(String bgdm) {
+		this.bgdm = bgdm;
+	}
+
+	public String getBgjc() {
+		return bgjc;
+	}
+
+	public void setBgjc(String bgjc) {
+		this.bgjc = bgjc;
+	}
+
+	public String getBgssrq() {
+		return bgssrq;
+	}
+
+	public void setBgssrq(String bgssrq) {
+		this.bgssrq = bgssrq;
+	}
+
+	public String getBgzgb() {
+		return bgzgb;
+	}
+
+	public void setBgzgb(String bgzgb) {
+		this.bgzgb = bgzgb;
+	}
+
+	public String getBgltgb() {
+		return bgltgb;
+	}
+
+	public void setBgltgb(String bgltgb) {
+		this.bgltgb = bgltgb;
+	}
+
+	public String getDldq() {
+		return dldq;
+	}
+
+	public void setDldq(String dldq) {
+		this.dldq = dldq;
+	}
+
+	public String getSheng() {
+		return sheng;
+	}
+
+	public void setSheng(String sheng) {
+		this.sheng = sheng;
+	}
+
+	public String getShi() {
+		return shi;
+	}
+
+	public void setShi(String shi) {
+		this.shi = shi;
+	}
+
+	public String getSshymc() {
+		return sshymc;
+	}
+
+	public void setSshymc(String sshymc) {
+		this.sshymc = sshymc;
+	}
+
+	public String getHttp() {
+		return http;
+	}
+
+	public void setHttp(String http) {
+		this.http = http;
+	}
+
+	@Override
+	public String toString() {
+		return "SZCompanyInfo [gsqc=" + gsqc + ", ywqc=" + ywqc + ", zcdz=" + zcdz + ", agdm=" + agdm + ", agjc=" + agjc
+				+ ", agssrq=" + agssrq + ", agzgb=" + agzgb + ", agltgb=" + agltgb + ", bgdm=" + bgdm + ", bgjc=" + bgjc
+				+ ", bgssrq=" + bgssrq + ", bgzgb=" + bgzgb + ", bgltgb=" + bgltgb + ", dldq=" + dldq + ", sheng="
+				+ sheng + ", shi=" + shi + ", sshymc=" + sshymc + ", http=" + http + "]";
+	}
+	
+	
+}
